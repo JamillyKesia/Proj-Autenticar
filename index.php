f9e6b0c0ad73fd931f1ffd2c181d83128cbf0032
Luan Costa <112595656+luuwn@users.noreply.github.com>
2023-05-16 17:25:33 -0300

    Upload do projeto
    Upload dos arquivos do projeto de segurança sobre autenticação de 2 fatores

---
+++ index.php
@@ -0,0 +1,104 @@
+<?php
+
+session_start();
+
+require_once('src/PHPMailer.php');
+require_once('src/SMTP.php');
+require_once('src/Exception.php');
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+// Verifica se o formulário foi enviado
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // Dados inseridos no formulário
+  $emailInserido = $_POST["email"];
+  $senhaInserida = $_POST["senha"];
+  $loginInserido = $_POST["login"];
+
+  // Email e senha já definidos
+  $loginCorreto = "UserTest";
+  $senhaCorreta = "senha123";
+
+  // Verifica se o email e a senha estão corretos
+  if ($loginInserido == $loginCorreto && $senhaInserida == $senhaCorreta) {
+    // Gera um código randômico de 6 dígitos
+    $codigoAutenticador = gerarCodigoAleatorio();
+
+    // Armazenar o código na variável de sessão
+    $_SESSION['codigoAcesso'] = $codigoAutenticador;
+
+    // Configurações do email
+    $mail = new PHPMailer(true);
+
+    try {
+      $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+      $mail->isSMTP();
+      $mail->Host = 'sandbox.smtp.mailtrap.io';
+      $mail->SMTPAuth = true;
+      $mail->Username = '1af160c49900f7';
+      $mail->Password = '4231f214464140';
+      $mail->Port = 2525;
+
+      // Configurações do servidor SMTP
+      $mail->setFrom('vocelo2442@appxapi.com');
+      //$mail->addAddress('Teste');
+      $mail->addAddress($emailInserido);
+    
+      $mail->isHTML(true);
+      $mail->Subject = 'Teste de email via gmail Canal TI';
+      $mail->Body = 'Sua senha de acesso é <strong>'.$codigoAutenticador.'</strong>';
+      $mail->AltBody = 'Chegou o email teste para 2 fatores';
+    
+      if($mail->send()) {
+        echo 'Email enviado com sucesso';
+
+        header("Location: validar.php");
+        exit();
+
+      } else {
+        echo 'Email nao enviado';
+      }
+    } catch (Exception $e) {
+      echo "Erro ao enviar mensagem: {$mail->ErrorInfo}";
+    }
+
+} else { echo 'Login ou Senha incorreta';}
+}
+
+// Função para gerar um código aleatório
+
+function gerarCodigoAleatorio() {
+  $codigo = rand(100000, 999999); // Gera um número aleatório de 6 dígitos
+  return $codigo;
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Formulário de Acesso</title>
+</head>
+<body>
+  <h2>Acesso</h2><br>
+  <form method="POST" action="">
+    <label for="email">Email:
+    <input type="email" id="email" name="email" required>
+    </label><br>
+    <label for="login">Login:
+      <input type="text" name="login" id="login">
+    </label><br>
+    <label for="senha">Senha:
+    <input type="password" id="senha" name="senha" required>
+    </label><br>
+
+    <input type="submit" value="Entrar">
+  </form><br>
+  LoginCorreto: UserTest
+  <br>
+  SenhaCorreta: senha123
+</body>
+</html>
---
+++ validar.php
@@ -0,0 +1,34 @@
+<?php
+
+session_start();
+
+echo "Código gerado: " . $_SESSION['codigoAcesso'];
+
+// Verificar se o código foi enviado pelo formulário
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  // Obter o código inserido pelo usuário
+  $codigoInserido = $_POST['codigoInserido'];
+
+  // Obter o código aleatório armazenado na variável de sessão
+  $codigoAutenticador = $_SESSION['codigoAcesso'];
+
+  // Verificar se o código inserido é igual ao código aleatório
+  if ($codigoInserido == $codigoAutenticador) {
+    echo "Código válido!"; // Código inserido está correto
+    
+    
+    header("Location: iupiii.html");
+        exit();
+
+} else {
+    echo "Código inválido!"; // Código inserido está incorreto
+  }
+}
+?>
+
+<!-- Formulário para inserir o código -->
+<form action="validar.php" method="post">
+  <label for="codigoInserido">Insira o código:</label>
+  <input type="text" name="codigoInserido" id="codigoInserido" required><br>
+  <button type="submit">Verificar Código</button>
+</form>
